295007c71033de0cb13c737200639caf74d16af2
SAI JAGADISH DARAPUREDDY <sai.jagadish.d@gmail.com>
2016-08-12 08:01:25 +0530

    Add files via upload

---
+++ Layout3.java
@@ -0,0 +1,20 @@
+package com.example.admin.listfragmentexample;
+
+/**
+ * Created by admin on 8/12/2016.
+ */
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Layout3 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+// TODO Auto-generated method stub
+        return  inflater.inflate(R.layout.layout3, container, false);
+    }
+}
+
